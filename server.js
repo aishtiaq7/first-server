9bc1930bb43335d3ce5085b85abb9d429e837bae
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-05-01 00:09:20 +0600

    rendered single static html file

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ console.log('\tServer started at port:' + port);
 
 // console.log('__dirname', __dirname);
 
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
 app.get("/", function (req, res) {
 
-    res.send('hello world');
+    res.render('index.html');
+    // res.send('hello world');
     // res.render(__dirname.toString() +'./public/index.html');
     // res.sendFile(path.join())
 });
