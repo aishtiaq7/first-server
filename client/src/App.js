54d650d161d4e427d61746da9bb2efd299676a39
ishmam8 <ishmam1998@gmail.com>
2021-05-03 13:54:58 -0700

    first_commit_ishmam

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
 class App extends Component {
+  state = { users: [] };
 
-  state = {users: []}
-
-  componentDidMount(){
-    fetch('http://localhost:3001/users')
-      .then(res => res.json())
-      .then(res => {
-        console.log(res); 
+  componentDidMount() {
+    fetch("http://localhost:3001/users")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
         return res;
       })
-      .then(users => this.setState({users}))
+      .then((users) => this.setState({ users }));
 
     //   .then(res = res.json())
     //   .then(users => this.setState({users}))
   }
 
-  render(){  
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <ul>
-            {
-              this.state.users.map((user) => (
-                
-                <li key={user.id}>{ `${user.id} ------ ${user.username}`}</li>
-        
-              ))
-            }
+            {this.state.users.map((user) => (
+              <li key={user.id}>{`${user.id} ------ ${user.username}`}</li>
+            ))}
           </ul>
         </header>
       </div>
     );
   }
 }
+//hello
 
 export default App;
