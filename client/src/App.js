567872621976dae9c608e07d44fd6253ec148d33
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-05-03 21:02:33 +0600

    connected react client side with backend server.js

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+
+  state = {users: []}
+
+  componentDidMount(){
+    fetch('http://localhost:3001/users')
+      .then(res => res.json())
+      .then(users => this.setState({users}))
+
+    //   .then(res = res.json())
+    //   .then(users => this.setState({users}))
+  }
+
+  render(){  
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <ul>
+              {this.state.users.map(user =>
+                <li key={user.id}>{user.username}</li>)
+              }
+             
+            </ul>
+          </header>
+        </div>
+      );
+  }
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,30 +1,51 @@
 const express = require("express");
 const app = express();
+const path = require ('path');
 
-const port = 3000; 
+const port = 3001; 
 console.log('\tServer started at port:' + port);
 
 // console.log('__dirname', __dirname);
 
-app.set('views', __dirname + '/views');
-app.engine('ejs', require('ejs').renderFile);
+app.set('views',path.join(__dirname, "views")); 
+app.set("view engine", 'ejs');
+
+app.use(express.static(__dirname +"/public"));
+
+
+app.get("/users", function (req, res) {
+    console.log('get - / logs');
+    var ejsVar = 'USEssssAME'; 
+    // res.render('index.ejs', { ejsVar }); 
+
+    // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+
+//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    res.json(
+        [
+            {id:1,username:"Awshaf"},
+            {id:2,username:"Fox"},
+
+        ]
+    )
+    // res.render(express.static('public'));
+
+});
+
+
 
 app.get("/", function (req, res) {
-    console.log('get - / logs');
-    
-    
-    var ejsVar = 'awshaf'; 
-    res.render('index.ejs', { ejsVar }); 
-    
-    
-});
-app.get("/gg", function (req, res) {
     console.log('get - / gg');
-
-    var ejsVar = 'gg'; 
+    var ejsVar = 'HELLO WORLD'; 
     res.render('index.ejs', { ejsVar }); 
-    
-    
 });
 
 app.listen(port);
