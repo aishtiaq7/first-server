89aa0c98b769ec7b8c24345207b96a18b6e5f4a6
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-05-23 09:51:52 +0600

    extracted values on submit

---
+++ client/src/Login.js
@@ -0,0 +1,44 @@
+import { React, useRef, useEffect, useState } from "react";
+import { Form, Button, Container } from "react-bootstrap";
+
+    export default function Login() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    useEffect(() => {
+        console.log(email);
+        console.log(password);
+    }, [email,password]);
+
+    const handleLogin = ()=>{
+        console.log('email:', email);
+        console.log('password:', password);
+    }
+
+    return (
+        <Container className="mt-5">
+        <Form>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" 
+                    value={email}
+                    onChange={ e=> setEmail(e.target.value)}
+                />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password"
+                value={password}
+                onChange={ e=> setPassword(e.target.value)} />
+            </Form.Group>
+            <Button variant="primary" type="submit"  onClick={()=>handleLogin()}>
+            Submit
+            </Button>
+        </Form>
+        </Container>
+    );
+}
