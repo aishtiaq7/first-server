6f541c731d2c74c1a13d6ad7f5718a09ce09815e
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-05-30 16:39:00 +0600

    cant view UserProfile if not signed in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
-import  React from "react";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Login from "./Login";
+import UserProfile from './UserProfile';
 
 function App() {
-  return (
-    // <h3>h</h3>
-    <Login />
-  )
+  return (<Router>
+    <Switch>
+      <Route exact path='/'><Login/></Route>
+    </Switch>
+    <Switch>
+      <Route exact path='/userProfile'>
+        <UserProfile/>
+      </Route>
+    </Switch>
+  </Router>);
 }
 
 export default App;
-
