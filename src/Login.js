4e66bcab7794cfca47b79fd256d493f850a122af
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-05-30 23:40:37 +0600

    cleanup code and config prettier extension

--- src/Login.js
+++ src/Login.js
@@ -3,154 +3,141 @@ import { Form, Button, Container } from "react-bootstrap";
 import { Redirect, useHistory } from "react-router-dom";
 import firebase from "firebase";
 
-
 var firebaseConfig = {
-  apiKey: "AIzaSyD046QyLkn90eFMNwV1mhWhC-hw_6RhQuY",
-  authDomain: "first-server-auth.firebaseapp.com",
-  projectId: "first-server-auth",
-  storageBucket: "first-server-auth.appspot.com",
-  messagingSenderId: "692608182980",
-  appId: "1:692608182980:web:3f5a2509c511cdf36bb791",
+    apiKey: "AIzaSyD046QyLkn90eFMNwV1mhWhC-hw_6RhQuY",
+    authDomain: "first-server-auth.firebaseapp.com",
+    projectId: "first-server-auth",
+    storageBucket: "first-server-auth.appspot.com",
+    messagingSenderId: "692608182980",
+    appId: "1:692608182980:web:3f5a2509c511cdf36bb791",
 };
 firebase.initializeApp(firebaseConfig);
 
 //Firebase AUTH change
 firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    var uid = user.uid;
-    console.log("\tAUTH CHANGE - Signed IN:");
-    console.log(uid);
-    // setIsLoggedIn(true);
-
-  } else {
-    console.log("\tAUTH CHANGE - NOTTTT  Signed IN:");
-    // setIsLoggedIn(false);
-
-    // history.push("/userprofile");
-    // <Redirect to="/" />
-  }
+    if (user) {
+        var uid = user.uid;
+        console.log("\tAUTH CHANGE - Signed IN:");
+        console.log(uid);
+    } else {
+        console.log("\tAUTH CHANGE - NOTTTT  Signed IN:");
+    }
 });
 
-// function firebasefunction(email, password) {
-  
-// }
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const history = useHistory();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const history = useHistory();
 
-  const credentials = useRef({
-    email: "defaulut email",
-    password: "default password",
-  });
+    const credentials = useRef({
+        email: "defaulut email",
+        password: "default password",
+    });
 
-  console.log("\n");
-  console.log("email:", email);
-  console.log("password:", password);
-  console.log("credendials", credentials.current);
-
-  const handleLogin = () => {
+    console.log("\n");
     console.log("email:", email);
     console.log("password:", password);
+    console.log("credendials", credentials.current);
 
-    credentials.current.email = email;
-    credentials.current.password = password;
-    console.log(credentials.current);
+    const handleLogin = () => {
+        console.log("email:", email);
+        console.log("password:", password);
 
+        credentials.current.email = email;
+        credentials.current.password = password;
+        console.log(credentials.current);
 
-    const loginPromise = firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        // Signed in
-        var user = userCredential.user;
-        console.log("..signed in with:");
-        console.log(user);
-        console.log('setting isLoggedIn'); 
-        setIsLoggedIn(true);
-        return userCredential;
-      })
-      .catch((error) => {
-        console.error('cauth error in signing in:', error)
-      });
-    loginPromise.then( uidPromise =>{
-      if(uidPromise == null){
-        setIsLoggedIn(false);
-      }
-    })
+        const loginPromise = firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+                console.log("..signed in with:");
+                console.log(user);
+                console.log("setting isLoggedIn");
+                setIsLoggedIn(true);
+                
+                history.push("/userprofile");
+                return userCredential;
+            })
+            .catch((error) => {
+                console.error("cauth error in signing in:", error);
+            });
+        loginPromise.then((uidPromise) => {
+            if (uidPromise == null) {
+                setIsLoggedIn(false);
+            }
+        });
 
-    
-    setEmail("");
-    setPassword("");
+        setEmail("");
+        setPassword("");
+    };
 
-  };
+    const handleLogout = () => {
+        firebase
+            .auth()
+            .signOut()
+            .then(() => {
+                // Sign-out successful.
+                console.log("logged out user");
+            })
+            .catch((error) => {
+                // An error happened.
+                console.log("error logging out");
+                console.log(error);
+            });
+    };
 
-  const handleLogout = () => {
-    firebase
-      .auth()
-      .signOut()
-      .then(() => {
-        // Sign-out successful.
-        console.log("logged out user");
-      })
-      .catch((error) => {
-        // An error happened.
-        console.log("error logging out");
-        console.log(error);
-      });
-  };
-
-  return (
-    <Container className="mt-5">
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button
-          variant="primary"
-          type="submit"
-          onClick={(e) => {
-            e.preventDefault();
-            handleLogin();
-          }}
-        >
-          Submit
-        </Button>
-        <Button
-          className="ml-2"
-          variant="primary"
-          type="button"
-          onClick={(e) => {
-            e.preventDefault();
-            // handleLogin();
-            handleLogout();
-          }}
-        >
-          Logout
-        </Button>
-      </Form>{
-        isLoggedIn && history.push("/userprofile")
-      }
-    </Container>
-  );
+    return (
+        <Container className="mt-5">
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Button
+                    variant="primary"
+                    type="submit"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleLogin();
+                    }}
+                >
+                    Submit
+                </Button>
+                <Button
+                    className="ml-2"
+                    variant="primary"
+                    type="button"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        // handleLogin();
+                        handleLogout();
+                    }}
+                >
+                    Logout
+                </Button>
+            </Form>
+        </Container>
+    );
 }
--- src/UserProfile.js
+++ src/UserProfile.js
@@ -2,35 +2,38 @@ import React, { Component } from "react";
 import firebase from "firebase";
 
 class UserProfile extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isLoggedIn: false,
-    };
-  }
-
-  componentDidMount() {
-    console.log("componentDidMount()");
-
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        // User logged in already or has just logged in.
-        console.log(user.uid);
-        this.setState({ isLoggedIn: true });
-      } else {
-        // User not logged in or has just logged out.
-        this.setState({ isLoggedIn: false });
-      }
-    });
-  }
-
-  render() {
-    if (this.state.isLoggedIn) {
-      return <h3>this is user profile</h3>;
+    constructor() {
+        super();
+        this.state = {
+            isLoggedIn: false,
+            uid:''
+        };
     }
 
-    return <h3>last else statement</h3>;
-  }
+    componentDidMount() {
+        console.log("componentDidMount()");
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                // User logged in already or has just logged in.
+                console.log(user.uid);
+                this.setState({ isLoggedIn: true });
+                this.setState({ uid: user.uid });
+            } else {
+                // User not logged in or has just logged out.
+                this.setState({ isLoggedIn: false });
+                this.setState({ uid: '' });
+
+            }
+        });
+    }
+
+    render() {
+        if (this.state.isLoggedIn) {
+            return <h3>{this.state.uid}</h3>;
+        }
+        return <h3>{'null'}</h3>;
+    }
 }
 
 export default UserProfile;
